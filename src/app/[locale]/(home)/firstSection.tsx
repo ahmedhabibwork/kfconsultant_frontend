da407b0761ae7f866c47f63d6bd80c27301d5f94
ahmednnasser11 <Ahmednnasser11@gmail.com>
2025-11-22 16:22:22 +0200

    feat: Add a reusable HeaderComponent and implement the first section of the home page using it.

--- src/app/[locale]/(home)/firstSection.tsx
+++ src/app/[locale]/(home)/firstSection.tsx
@@ -1,50 +1,20 @@
 "use client";
-import React from "react";
 import Image from "next/image";
-import { Search, Globe } from "lucide-react";
 import { motion } from "motion/react";
-import MenuButton from "@/components/cors/MenuButton";
-import Button from "@/components/button";
-import { Link } from "@/i18n/navigation";
+import HeaderComponent from "@/components/HeaderComponent";
 
 const FirstSection = () => {
   return (
-    <section
-      id="first-section"
-      className="panel w-full h-screen snap-start overflow-y-auto relative text-white"
-    >
+    <HeaderComponent background="/home-first-section.webp">
       <Image
-        alt="Background"
-        src="/home-first-section.webp"
-        fill
-        className="bg-image object-cover -z-20"
+        src="/logohome.webp"
+        alt="DMA Logo"
+        width={622}
+        height={156}
+        className="max-w-[466.5px] max-sm:max-w-[200px] z-10"
         priority
       />
-      <div className="bg-overlay bg-black/30 absolute inset-0"></div>
-
-      <div
-        className="z-10 flex flex-col h-[calc(100vh-270px)] justify-center items-center px-4 mt-[136px]"
-        data-aos="fade-up"
-        data-aos-once="true"
-        data-aos-duration="300"
-      >
-        <motion.div
-          initial={{ y: 100, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.8, ease: "easeOut" }}
-          className="flex justify-center"
-        >
-          <Image
-            src="/logohome.webp"
-            alt="DMA Logo"
-            width={622}
-            height={156}
-            className="max-w-[466.5px] max-sm:max-w-[200px] z-10"
-            priority
-          />
-        </motion.div>
-      </div>
-    </section>
+    </HeaderComponent>
   );
 };
 
---
+++ src/components/HeaderComponent.tsx
@@ -0,0 +1,45 @@
+"use client";
+import Image from "next/image";
+import { motion } from "motion/react";
+
+const HeaderComponent = ({
+  children,
+  background,
+}: {
+  children: React.ReactNode;
+  background: string;
+}) => {
+  return (
+    <section
+      id="first-section"
+      className="panel w-full h-screen snap-start overflow-y-auto relative text-white"
+    >
+      <Image
+        alt="Background"
+        src={background}
+        fill
+        className="bg-image object-cover -z-20"
+        priority
+      />
+      <div className="bg-overlay bg-black/30 absolute inset-0"></div>
+
+      <div
+        className="z-10 flex flex-col h-[calc(100vh-270px)] justify-center items-center px-4 mt-[136px]"
+        data-aos="fade-up"
+        data-aos-once="true"
+        data-aos-duration="300"
+      >
+        <motion.div
+          initial={{ y: 100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.8, ease: "easeOut" }}
+          className="flex justify-center"
+        >
+          {children}
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default HeaderComponent;
