5a52c7d7e5831a7db3232a14c87a60b7b921d7b6
ahmednnasser11 <Ahmednnasser11@gmail.com>
2025-11-23 23:55:33 +0200

    feat: Implement new About Us page with dedicated sections for awards, vision/mission, and sectors.

--- src/app/[locale]/(about)/about/page.tsx
+++ src/app/[locale]/(about)/about/page.tsx
@@ -4,15 +4,17 @@ import SecondSection from "./seconedSection";
 import ThiredSectionBg from "./ThiredSectionBg";
 import Sectors from "./sectors";
 import AwardsSection from "./AwardsSection";
+import Footer from "@/components/cors/footer";
 
 const page = () => {
   return (
-    <div>
+    <div className="h-screen w-full overflow-y-scroll snap-y snap-mandatory scroll-smooth no-scrollbar">
       <AboutHeader />
       <SecondSection />
       <ThiredSectionBg />
       <Sectors />
       <AwardsSection />
+      <Footer />
     </div>
   );
 };
